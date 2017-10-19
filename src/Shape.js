b32589fdba71390b863f37ae482bce0482aed4a3
Anton Emelyanov <anton@mediio.net>
2017-10-19 11:45:32 +0900

    Shape positioning moved to css

--- src/Shape.js
+++ src/Shape.js
@@ -8,15 +8,13 @@ import React, { Component } from 'react';
 class Shape extends Component {
   render() {
     const options = this.props.options;
-    const top = (options.y - options.width/2);
-    const left = (options.x - options.width/2);
 
     const style = {
       backgroundColor: options.color,
       width: options.width + 'px',
       height: options.width + 'px',
-      top: top + 'px',
-      left: left + 'px',
+      top: options.y + 'px',
+      left: options.x + 'px',
     };
 
     return (
